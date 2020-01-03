4be2d9f2ad77833ec5e9be365e84e0106f8a1f6f
Mahmoud Gudarzi <mahmoud.gudarzi@gmail.com>
2020-01-03 13:27:56 -0500

    Update setup_jupyter.sh

--- setup_jupyter.sh
+++ setup_jupyter.sh
@@ -4,7 +4,7 @@ wget --no-check-certificate https://repo.continuum.io/miniconda/Miniconda3-lates
 bash Miniconda3-latest-Linux-x86_64.sh -b -p /opt/anaconda3
 echo "PATH=/opt/anaconda3/bin:$PATH" | sudo tee -a /etc/environment
 source /etc/environment
-conda install -c conda-forge notebook
 conda install -c anaconda pandas
 conda install -c anaconda numpy
-conda install -c anaconda seaborn 
+conda install -c anaconda seaborn
+conda install -c conda-forge notebook
